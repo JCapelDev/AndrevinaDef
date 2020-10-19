e70fb1e0a21b089d416a1e14de25288c3c8826eb
JCapelDev <win200003@gmail.com>
2020-10-19 16:01:48 +0200

    commit 19 oct

--- app/src/main/java/com/example/empty1/MainActivity.java
+++ app/src/main/java/com/example/empty1/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.empty1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -93,6 +97,30 @@ public class MainActivity extends AppCompatActivity {
         String message = String.valueOf(cont);
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+
+    }
+
+    public class rankInsc extends DialogFragment{
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.rankInsc)
+                    .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // FIRE ZE MISSILES!
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
+
+
 
     }
 
--- app/src/main/java/com/example/empty1/RankingActivity.java
+++ app/src/main/java/com/example/empty1/RankingActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -26,18 +27,18 @@ public class RankingActivity extends AppCompatActivity {
 
         // Capture the layout's TextView and set the string as its text
 
-        TextView textView = findViewById(R.id.textView2);
+       // TextView textView = findViewById(R.id.textView2);
 //        textViewName.setText("Proba");
 //        textViewTime.setText("0.0");
 //        textViewPoints.setText("aL.get(i).getPoints()");
-        TableLayout tableLayout1 = (TableLayout)findViewById(R.id.tableLayout1);
-        for (int i = 0; i < MainActivity.aL.size(); i++) {
+       // TableLayout tableLayout1 = (TableLayout)findViewById(R.id.tableLayout1);
+      //  for (int i = 0; i < MainActivity.aL.size(); i++) {
 
 
 //            textViewName.setText(aL.get(i).getName());
 //            textViewTime.setText(aL.get(i).getTime().toString());
 //            textViewPoints.setText(aL.get(i).getPoints().toString());
-        }
+    //    }
 
         /*
         aL.add(message);
@@ -47,10 +48,39 @@ public class RankingActivity extends AppCompatActivity {
         }
 
          */
+        //make sure that the lists contain data or else display will be blank screen
+
+        TableRow.LayoutParams  params1=new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT,1.0f);
+        TableRow.LayoutParams params2=new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+        TableLayout tbl=(TableLayout) findViewById(R.id.tlMarksTable);
+
+       for (int i = 0; i < MainActivity.aL.size() ; i++) {
+
+            //Creating new tablerows and textviews
+            TableRow row=new TableRow(this);
+            TextView txt1=new TextView(this);
+            TextView txt2=new TextView(this);
+            TextView txt3=new TextView(this);
+            //setting the text
+            txt1.setText(aL.get(i).getName());
+            txt2.setText(aL.get(i).getPoints().toString());
+            txt3.setText(aL.get(i).getTime().toString());
+            txt1.setLayoutParams(params1);
+            txt2.setLayoutParams(params1);
+            txt3.setLayoutParams(params1);
+            //the textviews have to be added to the row created
+            row.addView(txt1);
+            row.addView(txt2);
+            row.addView(txt3);
+            row.setLayoutParams(params2);
+            tbl.addView(row);
+        }
 
 
 
     }
+
+
     public void sendMessage(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, MainActivity.class);
